69cb385c6856e2675b46169b1ac87c10fc9173f7
Bay Bui <baybui@ucla.edu>
2022-07-23 15:43:27 -0700

    added popular meals

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("test");
+const icons = document.querySelectorAll(".section-1-icons i");
+
+let iconCount = 1;
+
+setInterval(() => {
+  iconCount++;
+  const icon = document.querySelector(".section-1-icons .change");
+  icon.classList.remove("change");
+
+  if (iconCount > icons.length) {
+    icons[0].classList.add("change");
+    iconCount = 1;
+  } else {
+    icon.nextElementSibling.classList.add("change");
+  }
+}, 1000);
